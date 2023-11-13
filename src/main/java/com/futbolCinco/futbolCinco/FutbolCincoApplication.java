319b1d3c123025f01896250480ed4abc5e6aa53d
ChernoAlpha96 <fede.p-@hotmail.com>
2023-11-13 17:03:39 -0300

    commit inicial

---
+++ src/main/java/com/futbolCinco/futbolCinco/FutbolCincoApplication.java
@@ -0,0 +1,13 @@
+package com.futbolCinco.futbolCinco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolCincoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolCincoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futbolCinco/futbolCinco/FutbolCincoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futbolCinco.futbolCinco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolCincoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
